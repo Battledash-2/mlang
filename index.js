8f8a6a9fff1834061bd156a6575c651a62bcd73d
Unzor <71789103+Unzor@users.noreply.github.com>
2022-01-13 19:28:44 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,58 @@
+function isInArray(str, array) {
+    return array.indexOf(str) !== -1
+}
+
+function cliParser(cmd, settings) {
+    var flags = [];
+    var def = [];
+    var doNotPush = [];
+    global.parsed = {};
+    var splitCommands = cmd.split(" ");
+    cmd.split(" ").forEach(function(arg, indexer) {
+        if (arg.startsWith("--") || arg.startsWith("-")) {
+            if (settings) {
+                if (settings.readCommandAfter) {
+                    if (isInArray(arg, settings.readCommandAfter)) {
+                        flags.push(arg + " " + splitCommands[indexer + 1]);
+                        doNotPush.push(splitCommands[indexer + 1]);
+                    } else {
+                        if (!isInArray(splitCommands[indexer + 1], doNotPush))  {
+                            flags.push(arg);
+                        }
+                    }
+                }
+            }
+        } else {
+            if (!isInArray(arg, doNotPush)) {
+                def.push(arg);
+            }
+        }
+        if (indexer + 1 == cmd.split(" ").length) {
+            parsed = {
+                commands: def,
+                flags: flags
+            }
+        }
+    })
+    return parsed;
+}
+
+
 const fs = require("fs");
 const readline = require("readline");
 
-const mode = process.argv[2];
+var cli = cliParser(process.argv.slice(2).join(" "), {
+readCommandAfter: ["-m", "--mode"]
+});
+
+var mode;
+if (cli.commands[0]) {
+mode = cli.flags[0].split(" ").pop() || "f"
+}
+else {
+mode = "c"
+}
+
 const filename  = process.argv[3]; // file or terminal / console
 
 const ExecProc = require("./ExecProc");
@@ -9,7 +60,7 @@ const ExecProc = require("./ExecProc");
 if (mode == null) {
     console.error("Missing argument 1");
 } else {
-    if (mode.toLowerCase() == "-f") {
+    if (mode.toLowerCase() == "f") {
         if (fs.existsSync(filename)) {
             // console.log(String(fs.readFileSync(filename)));
             const filecontent = String(fs.readFileSync(filename));
@@ -18,7 +69,7 @@ if (mode == null) {
         } else {
             console.error(`File '${filename}' does not exist.`);
         }
-    } else if(mode.toLowerCase() == "-c") {
+    } else if(mode.toLowerCase() == "c") {
         const interface = readline.createInterface({
             input: process.stdin,
             output: process.stdout
