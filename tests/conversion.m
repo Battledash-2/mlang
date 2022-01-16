eda8a02f9cf39f99b11c5ce42dfafb956d5ccebb
Battledash-2 <69129349+Battledash-2@users.noreply.github.com>
2022-01-15 21:05:50 -0500

    Added conversion test

---
+++ tests/conversion.m
@@ -0,0 +1,10 @@
+// fahrenheit to celsius
+util.log(6 => f, c)
+
+// miles to kilometers / kilometers to miles
+util.log(16 => mi, km)
+util.log(16 => km, mi)
+
+// meters to kilometres / meters to miles
+util.log(5000 => m, km) // 5
+util.log(5000 => m, mi) // 3
