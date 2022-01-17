467b0ad081ae1033ccc72359a83ac874d45679d3
Battledash-2 <69129349+Battledash-2@users.noreply.github.com>
2022-01-16 22:12:46 -0500

    new example

---
+++ example1.m
@@ -0,0 +1,10 @@
+import "./tests/imports_greet.m"
+
+func sayHelloTo {
+    util.log("Hello, " + util.arg + "!")
+}
+
+sayHelloTo("Nemo") // Hello, Nemo!
+
+let greeting = "Hey"
+util.log(greet("Dora")) // Hey, Dora!
