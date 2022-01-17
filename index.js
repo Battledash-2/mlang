3092a1198b7463e8c5db62033f5abc0b733d45d5
Battledash-2 <69129349+Battledash-2@users.noreply.github.com>
2022-01-17 09:47:54 -0500

    remove newline

--- index.js
+++ index.js
@@ -20,8 +20,7 @@ if (mode == null) {
         }
     } else if(mode.toLowerCase() == "-c") {
         console.log(`MLang v${require("./package.json").version}
-Copyright (c) 2022 Battledash-2 (& MLang)
-`);
+Copyright (c) 2022 Battledash-2 (& MLang)`);
 
         const interface = readline.createInterface({
             input: process.stdin,
