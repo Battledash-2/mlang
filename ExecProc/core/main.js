066d09b3a18f6c8e5fe8ea4b259d08a4bd010e10
Battledash-2 <69129349+Battledash-2@users.noreply.github.com>
2022-01-17 18:39:44 -0500

    New `util.len` for string lengths and new test for conditional statements

--- ExecProc/core/main.js
+++ ExecProc/core/main.js
@@ -6,6 +6,7 @@ module.exports = (createToken)=>{
         "util.log": (arg)=>console.log(arg.value),
         "util.sin": (arg, pos)=>createToken("NUMBER", Math.sin(arg.value), pos.position),
         "util.cosin": (arg, pos)=>createToken("NUMBER", Math.cos(arg.value), pos.position),
+        "util.len": (arg, pos)=>createToken("NUMBER", String(arg.value).length, pos.position),
         // ...require("./convert")(createToken)
     }
 };
---
+++ tests/conditional_with_strings.m
@@ -0,0 +1,11 @@
+func isOver5Char {
+    if (util.len(util.arg) > 5) {
+        util.log("OVER 5")
+    } else {
+        util.log("UNDER 5")
+    }
+}
+
+isOver5Char("poo") // no (3 chars)
+isOver5Char("poop5") // no (5 chars [we're using the more than symbol rather then the more than or equal to symbol])
+isOver5Char("poop45") // yes (6 chars)
