f9188a1db99ce03c8d53780aec110552868eca94
Battledash-2 <69129349+Battledash-2@users.noreply.github.com>
2022-01-18 22:59:54 -0500

    boilerplate

---
+++ ExecProc/core/modules/boilerplate.js
@@ -0,0 +1,9 @@
+const Interface = require("<path-to-interface>");
+
+module.exports = class <module-name> extends Interface {
+    constructor(...args) {
+        super(...args);
+
+        // use `this.createFunction` or other functions here (see full list in <path-to-interface>)
+    }
+}
