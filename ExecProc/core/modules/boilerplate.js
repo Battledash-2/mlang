21d851f6179ceda3b76ff77ea3b065ea34c1b28b
Battledash-2 <69129349+Battledash-2@users.noreply.github.com>
2022-01-18 23:02:36 -0500

    more boilerplate

--- ExecProc/core/modules/boilerplate.js
+++ ExecProc/core/modules/boilerplate.js
@@ -5,5 +5,13 @@ module.exports = class <module-name> extends Interface {
         super(...args);
 
         // use `this.createFunction` or other functions here (see full list in <path-to-interface>)
+        this.createFunction("module-name::my-function-name", (arg)=>{
+            if (Array.isArray(arg)) {
+                console.log("Multiple arguments", arg[0].value);
+            } else if (arg != null) {
+                console.log("Single argument", arg.value);
+            }
+            return this.createToken("TYPE", "VALUE", this.getPositionObject());
+        });
     }
 }
