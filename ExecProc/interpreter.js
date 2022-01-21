bd8e5670eddc2dccc61b16604a142a640f348977
Battledash-2 <69129349+Battledash-2@users.noreply.github.com>
2022-01-21 11:38:07 -0500

    error prevent

--- ExecProc/interpreter.js
+++ ExecProc/interpreter.js
@@ -383,7 +383,7 @@ module.exports = class Interpreter {
 
 		if (node?.type == "IMPORT") {
 			this.pos = node;
-			if (!node?.file.endsWith(".js") && fs.existsSync(node?.file)) { // endswith first so we're not checking if it exists for nothing
+			if (!node?.file?.endsWith(".js") && fs.existsSync(node?.file)) { // endswith first so we're not checking if it exists for nothing
 				this.importFile(node);
 			} else if (fs.existsSync(node?.file)) {
 				const userModule = require(__dirname, node?.file);
