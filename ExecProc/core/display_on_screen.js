e37ee1e6d3c1d62df0375a51030fc3f86a0695b2
Battledash-2 <69129349+Battledash-2@users.noreply.github.com>
2022-01-23 15:17:49 -0500

    update display_on_screen to work with identifiers

--- ExecProc/core/display_on_screen.js
+++ ExecProc/core/display_on_screen.js
@@ -5,18 +5,22 @@ const form = (mod)=>{
 		for (let k in mod) {
 			let v = mod[k];
 			if (k == mod.length-1) {
-				if (typeof v == "string") {res+=`"${v}`;continue};
+				if (v?.type == "IDENTIFIER") {res+=`${form(v?.value)}`;continue;}
+				if (typeof v?.value == "string" || typeof v == "string") {res+=`"${v?.value ?? v}"`;continue};
 				res+=`${form(v)}`;
 				continue;
 			}
-			if (typeof v == "string") {res+=`"${v}", `;continue};
+			if (v?.type == "IDENTIFIER") {res+=`${form(v?.value)}, `;continue;}
+			if (typeof v?.value == "string" || typeof v == "string") {res+=`"${v?.value ?? v}", `;continue};
 			res+=`${typeof form(v) == "object" ? form(form(v)) : form(v)}, `;
 		}
 		
 		res += " ]";
-		return res;
+		return res.replace(/\s{1},\s{1}/, " ");
 	} else {
-		return mod?.value || mod;
+		if (mod?.type == "IDENTIFIER") {return `${form(mod?.value)}`;}
+		if (typeof mod?.value == "string" || typeof mod == "string") {return `"${mod?.value ?? mod}", `;};
+		return mod?.value ?? mod;
 	}
 }
 
