f8da59ead48d0a9b743b8b5ae27e3158c2ba3789
Battledash-2 <69129349+Battledash-2@users.noreply.github.com>
2022-01-26 17:21:59 -0500

    Array test

---
+++ tests/array_assign.m
@@ -0,0 +1,17 @@
+useStrict();
+
+const nestedArray = ["poop", 43, ["pop", 2], ["nested", ["array", [":("]]]];
+print(nestedArray); // [ poop, 43, [ "pop", 2 ], [ "nested", [ "array", [ ":(" ] ] ] ]
+
+nestedArray[0] = "not poop"
+print(nestedArray); // [ not poop, 43, [ "pop", 2 ], [ "nested", [ "array", [ ":(" ] ] ] ]
+
+nestedArray[2][1] = 5;
+print(nestedArray); // [ poop, 43, [ "pop", 5 ], [ "nested", [ "array", [ ":(" ] ] ] ]
+
+nestedArray[3][1][1][0] = ":)";
+print(nestedArray); // [ poop, 43, [ "pop", 5 ], [ "nested", [ "array", [ ":)" ] ] ] ]
+
+// you must use brackets to use an expression within the item selector
+nestedArray[2][(0+1)] = 7;
+print(nestedArray); // [ poop, 43, [ "pop", 7 ], [ "nested", [ "array", [ ":)" ] ] ] ]
