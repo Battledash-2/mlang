31ed5b085091d0c0ceb83614e91613644d2a0eff
Battledash-2 <69129349+Battledash-2@users.noreply.github.com>
2022-01-26 17:22:24 -0500

    expand on comment

--- tests/array_assign.m
+++ tests/array_assign.m
@@ -12,6 +12,6 @@ print(nestedArray); // [ poop, 43, [ "pop", 5 ], [ "nested", [ "array", [ ":(" ]
 nestedArray[3][1][1][0] = ":)";
 print(nestedArray); // [ poop, 43, [ "pop", 5 ], [ "nested", [ "array", [ ":)" ] ] ] ]
 
-// you must use brackets to use an expression within the item selector
+// you must use brackets to use an expression within the item selector (otherwise it will error)
 nestedArray[2][(0+1)] = 7;
 print(nestedArray); // [ poop, 43, [ "pop", 7 ], [ "nested", [ "array", [ ":)" ] ] ] ]
