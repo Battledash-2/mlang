116c7c861ab047cc9a7df13315a366dde2306351
Battledash-2 <69129349+Battledash-2@users.noreply.github.com>
2022-01-27 20:47:41 -0500

    no isnan

--- ExecProc/core/main.js
+++ ExecProc/core/main.js
@@ -92,7 +92,6 @@ module.exports = (fn)=>{
 
 		"util.argv": process.argv.map(c=>({type:"STRING",value:c,position:{line:0,cursor:0}})),
 
-		"isNaN": (arg, pos)=>createToken("BOOLEAN", isNaN(arg.value), pos.position),
 		"isNull": (arg, pos)=>createToken("BOOLEAN", arg == null || arg.value == null, pos.position),
 		// ...require("./convert")(createToken)
 	}
